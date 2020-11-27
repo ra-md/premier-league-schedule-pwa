6c349cbe5f4a97fdc1983729463330278a2e259e
ra-md <ra799m@gmail.com>
2020-11-27 14:06:37 +0700

    implement load more button

--- js/pages/loadJadwal.js
+++ js/pages/loadJadwal.js
@@ -2,49 +2,37 @@ import api from '../api/api.js';
 import loading from '../utils/loading.js';
 import db from '../utils/db.js';
 
-const API_URL = 'https://api.football-data.org/v2';
+let matches;
+let start = 0;
+let end = 9;
 
-function loadJadwal() {
-	const content = document.getElementById('jadwal');
+async function renderMatches() {
+	if (end < matches.length) {
+		for(let i = start; i < end; i++) {
+			const match = matches[i];
 
-	if(content) {
-		loading(content);
+			const convertToLocal = new Date(match.utcDate);
+			const date = convertToLocal.toString().split(' ').splice(0,5).join(' ');
 
-		api.getSchedule()
-			.then(async matches => {
+			const apakahSudahAdaDiIndexedDB = await db.getById(match.id.toString());
 
-				loading(content, '');
+			document.getElementById('jadwal-list').innerHTML += `
+				<div class="col s6 responsive">
+					<div class="card">
+						<h6 class="left m-2">${date}</h6>
+						<button ${apakahSudahAdaDiIndexedDB ? 'disabled':''} id="${match.id}" class="right m-2 btn-simpan">Simpan</button>
+				    <div class="card-content">
+				    	<h5 class="center-align">${match.homeTeam.name}</h5>
+		    			<h6 class="center-align">vs</h6>
+				    	<h5 class="center-align">${match.awayTeam.name}</h5>
+				    </div>
+				  </div>
+				</div>`;
+		}
 
-				if(matches.length !== 0) {
-					for(let match of matches) {
-						const convertToLocal = new Date(match.utcDate);
-						const date = convertToLocal.toString().split(' ').splice(0,5).join(' ');
+		saveMatch();
+	}
 
-						const apakahSudahAdaDiIndexedDB = await db.getById(match.id.toString());
-
-						content.innerHTML += `
-							<div class="col s6 responsive">
-								<div class="card">
-									<h6 class="left m-2">${date}</h6>
-									<button ${apakahSudahAdaDiIndexedDB ? 'disabled':''} id="${match.id}" class="right m-2 btn-simpan">Simpan</button>
-							    <div class="card-content">
-							    	<h5 class="center-align">${match.homeTeam.name}</h5>
-					    			<h6 class="center-align">vs</h6>
-							    	<h5 class="center-align">${match.awayTeam.name}</h5>
-							    </div>
-							  </div>
-							</div>`;
-					};
-				} else {
-					content.innerHTML = '<h5 class="center-align">Musim 2018/2019 telah berakhir.</h5>';
-				}
-
-
-				saveMatch(matches);
-
-			})
-
-	};
 };
 
 function saveMatch() {
@@ -67,6 +55,47 @@ function saveMatch() {
 			btn.setAttribute('disabled', '');
 		});
 	});
+};
+
+function loadMore() {
+	const loadMoreBtn = document.getElementById('load-more-btn');
+
+	loadMoreBtn.addEventListener('click', () => {
+		if (end < matches.length) {
+			start += 10;
+			end += 10;
+			renderMatches();
+
+			if (end === matches.length - 1) {
+				loadMoreBtn.setAttribute('disabled', '');
+			}
+		}
+	})
+};
+
+async function loadJadwal() {
+	const jadwalContainer = document.getElementById('jadwal-container');
+
+	start = 0;
+	end = 9;
+
+	if(jadwalContainer) {
+		loading(jadwalContainer);
+
+		matches = await api.getSchedule();
+
+		loading(jadwalContainer, `
+			<div class="row" id="jadwal-list"></div>
+		  <button id="load-more-btn" class="waves-effect waves-light btn m-2 x-center">Load more</button>
+		`);
+
+		if(matches.length !== 0) {
+			renderMatches();
+			loadMore();
+		} else {
+			jadwalContainer.innerHTML = '<h5 class="center-align">Musim 2018/2019 telah berakhir.</h5>';
+		}		
+	};
 };
 
 export default loadJadwal;
